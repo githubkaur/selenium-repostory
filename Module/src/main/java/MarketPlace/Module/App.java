3d8b31f4f36802d547386a805e9c47c7663fd9b9
mindQ <hp1>
2018-06-24 20:56:12 +0530

    My  Practice project sale Project

---
+++ Module/src/main/java/MarketPlace/Module/App.java
@@ -0,0 +1,13 @@
+package MarketPlace.Module;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
